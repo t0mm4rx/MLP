0671979b332c615cb48927997a2570e9dc35f8bf
t0mm4rx <tom.marx@hotmail.fr>
2018-11-25 20:02:07 +0100

    Working network for one neuron with two inputs

--- Network.py
+++ Network.py
@@ -1,90 +1,42 @@
-# coding: utf-8
 import numpy as np
-from Layer import Layer
 
 class Network:
 
-    def __init__ (self, inputs_n, hiddens, outputs):
-        self.learning_rate = 0.1
-        self.layers = []
-        for i in range(len(hiddens)):
-            if (i == 0):
-                self.layers.append(Layer(inputs_n, hiddens[i]))
-            else:
-                self.layers.append(Layer(hiddens[len(hiddens) - 2], hiddens[i]))
-        self.layers.append(Layer(hiddens[len(hiddens) - 1], outputs))
+    def __init__(self):
+        self.learning_rate = .1
+        self.w1 = (np.random.random() - 0.5) * 2
+        self.w2 = (np.random.random() - 0.5) * 2
+        self.b = (np.random.random() - 0.5) * 2
 
-    # On donne une entrée au réseau qui réalise une prédiction
-    def process(self, inputs):
-        prev_output = inputs
-        for l in self.layers:
-            prev_output = l.process(prev_output)
-        return prev_output
+    def guess(self, inputs):
+        return self.sigmoid(self.w1 * inputs[0] + self.w2 * inputs[1] + self.b)
 
-    # Même fonction que process mais on enegistre le résultat de tous les layers, pour la rétropropagation
-    def process_all(self, inputs):
-        prev_output = [len(self.layers) + 1]
-        prev_output[0] = inputs
-        for l in self.layers:
-            prev_output.append(l.process(prev_output[len(prev_output) - 1]))
-        return prev_output
+    def train(self, inputs):
+        s = self.w1 * inputs[0] + self.w2 * inputs[1] + self.b
+        pred = self.sigmoid(s)
+        target = inputs[2]
+        cost = self.cost(pred, target)
+        delta = self.delta(pred, target)
 
-    # On donne une entrée et la sortie associée, le réseau s'améliore
-    def train(self, inputs, outputs):
-        guess = self.process_all(inputs)
-        # On calcule l'erreur de tous les layers
-        error_output = np.subtract(outputs, guess[len(guess) - 1])
-        error = self.unactivate(error_output)
-        #error = error_output * -1
-        #error *= np.sign(error_output) * -1
+        der = self.sigmoid_derivate(pred)
 
-        errors = [[]] * len(self.layers)
-        errors[len(self.layers) - 1] = error
-        for a in range(len(self.layers) - 1):
-            layer = self.layers[len(self.layers) - a - 1]
-            previous_layer = self.layers[len(self.layers) - a - 2]
-            previous_error = [0] * previous_layer.neurons_n
-
-            for i in range(previous_layer.neurons_n):
-                e = 0
-                for n in range(layer.neurons_n):
-                    e += errors[len(self.layers) - a - 1][n] * np.absolute(layer.weights[n][i])
-                previous_error[i] = self.unactivate(e) * np.sign(e)
-
-            errors[len(self.layers) - a - 2] = previous_error
-
-        print(str(guess[len(guess) - 1]) + " / " + str(outputs))
-        print(errors)
-
-        # On corrige les poids et les bias
-        for a in range(len(self.layers)):
-            l = len(self.layers) - a - 1
-            layer = self.layers[l]
-            for n in range(layer.neurons_n):
-                # Les poids
-                for i in range(layer.inputs_n):
-                    delta = errors[l][n] * -1
-                    delta *= np.absolute(layer.weights[n][i])
-                    delta *= np.absolute(self.unactivate(guess[l][i]))
-                    delta *= self.learning_rate
-                    layer.weights[n][i] += delta
-                    layer.weights[n][i] = np.clip(layer.weights[n][i], -500, 500)
-            # Les bias
-            bias_error = 0
-            for n in range(layer.neurons_n):
-                bias_error += errors[l][n]
-            #bias_error = -1 * np.sign(bias_error) * bias_error ** 2
-            layer.bias += bias_error * np.absolute(layer.bias) * self.learning_rate * -1
-            layer.bias = np.clip(layer.bias, -500, 500)
+        self.w1 += delta * der * inputs[0]
+        self.w2 += delta * der * inputs[1]
+        self.b += delta * der
+        return cost
 
 
-        return np.linalg.norm(error)
+    def cost(self, prediction, expected):
+        return (expected - prediction) ** 2
 
+    def cost_slope(self, prediction, expected):
+        return 2 * (prediction - expected)
 
-    def unactivate(self, n):
-        #return 1 * (1 - np.clip(n, -500, 500))
-        return self.activate(n) * (1 - self.activate(n))
+    def delta(self, prediction, expected):
+        return self.cost_slope(prediction, expected) * -self.learning_rate
 
-    def activate(self, input):
-        #return 1 / (1 + np.exp(-np.clip(input, -500, 500)))
-        return 1 / (1 + np.exp(-input))
+    def sigmoid (self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_derivate(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# coding: utf-8
-import math
-import random
 from Network import Network
+import matplotlib.pyplot as plt
+import numpy as np
+import pyttsx3
 
-net = Network(2, [4, 2], 1)
+data = [
+    [4, 3, 0],
+    [2, 2, 0],
+    [3, 1, 0],
+    [5, 3, 0],
+    [3, 3, 0],
+    [4, 2, 0],
+    [1, 4, 0],
+    [-1, 0, 1],
+    [1, 0, 1],
+    [0, -2, 1],
+    [-2, 1, 1],
+    [1, -1, 1],
+    [1, 1, 1],
+    [2, -1, 1]
+]
 
-#for _ in range(10000000):
-#    print(net.train([1, 0, 0], [0, 1]))
+to_guess = [3, 1]
 
-for _ in range(10000):
-    print(net.train([0, 1], [0]))
+net = Network()
 
-print()
-print("Guess [0, 1]")
-print(net.process([0, 1]))
+costs = []
+for _ in range(100000):
+    index = np.random.randint(len(data))
+    point = data[index]
+    costs.append(net.train(point))
+
+plt.plot(costs)
+plt.show()
+
+"""
+res = net.guess(to_guess)
+if (res > .5):
+    color = "blue"
+else:
+    color = "red"
+
+engine = pyttsx3.init()
+engine.say(color)
+engine.runAndWait()
+
+for a in data:
+    color = 'ro'
+    if (a[2] == 1):
+        color = 'bo'
+    plt.plot(a[0], a[1], color)
+
+plt.plot(to_guess[0], to_guess[1], 'yo')
+plt.show()
+"""
